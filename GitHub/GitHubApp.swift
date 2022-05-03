91f89699515a16df82d2c40e118a300c2b2b5917
RixinLi <rli140@ucsc.edu>
2022-05-02 19:46:17 -0700

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Rixin Li on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
